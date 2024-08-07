c7161ed77c2bd4114e27a4a629d2c1b706366152
Alfonso Aguilar <aruponse@gmail.com>
2024-08-06 20:50:32 -0500

    Fix Syntaxt errors

--- WebApp/Service/ImportadorService.cs
+++ WebApp/Service/ImportadorService.cs
@@ -258,11 +258,11 @@ namespace WebApp.Service.IService
         string newSelectFieldsStr = string.Join(", ", newSelectFields);
        
         if (fieldExists(connection, viewName, idEnteName)) {
-            newSelectFieldsStr += ", IdEnte";
+            newSelectFieldsStr += $", {idEnteName}";
             saveIdEnte = true;
         }
         else {
-            Console.WriteLine($"Field {idEnteName} does not exist in view {viewName)}");
+            Console.WriteLine($"Field {idEnteName} does not exist in view {viewName}");
             saveIdEnte = false;
         }
         if (fieldExists(connection, viewName, vids[executionIndex])) {
