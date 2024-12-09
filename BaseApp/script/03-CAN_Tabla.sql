7f176cc04e2e26a984b99b02d8ca1d29eb3ea221
pat_mic <pat_mic@hotmail.com>
2024-12-09 14:03:01 -0500

    Tablas log y vistas

--- BaseApp/script/03-CAN_Tabla.sql
+++ BaseApp/script/03-CAN_Tabla.sql
@@ -186,37 +186,114 @@ CREATE TABLE dbo.WebSiteLog (
 	,CONSTRAINT  [PK_WSL_WebSiteLog]		PRIMARY KEY CLUSTERED (IdWebSiteLog) 
 );
 
+DROP TABLE if exists [dbo].[ONA] ;
+CREATE TABLE [dbo].[ONA] (
+     IdONA			    INT IDENTITY(1,1)
+    ,IdHomologacion	    INT NOT NULL
+    ,Siglas			    NVARCHAR(30)  NOT NULL
+    ,RazonSocial		NVARCHAR(300) NOT NULL
+    ,Pais			    NVARCHAR(100) NOT NULL
+    ,Ciudad			    NVARCHAR(100) NOT NULL
+    ,Direccion		    NVARCHAR(300) NOT NULL
+    ,PaginaWeb		    NVARCHAR(300) NOT NULL
+    ,Correo			    NVARCHAR(300) NOT NULL
+    ,Telefono		    NVARCHAR(20)  NOT NULL
+    ,UrlIcono		    NVARCHAR(300) NOT NULL
+    ,UrlLogo			NVARCHAR(300) NOT NULL
+
+    ,Estado				NVARCHAR(1) NOT NULL DEFAULT('A')
+    ,FechaCreacion		DATETIME	NOT NULL DEFAULT(GETDATE())
+    ,FechaModifica		DATETIME	NOT NULL DEFAULT(GETDATE())  
+    ,IdUserCreacion		INT			NOT NULL DEFAULT(0)
+    ,IdUserModifica		INT			NOT NULL DEFAULT(0)  
+  
+    ,CONSTRAINT  [PK_O_IdONA]		    PRIMARY KEY CLUSTERED (IdONA) 
+    ,CONSTRAINT   FK_O_IdHomologacion	FOREIGN KEY (IdHomologacion) REFERENCES Homologacion(IdHomologacion)
+    ,CONSTRAINT  [CK_O_Estado]		    CHECK   (Estado IN ('A', 'X'))
+  
+);
+
+
+DROP TABLE if exists [dbo].[EsquemaVista] ;
+CREATE TABLE [dbo].[EsquemaVista] (
+     IdEsquemaVista			    INT IDENTITY(1,1)
+    ,IdConexion                 INT NOT NULL
+    ,IdHomologacionEsquema	    INT NOT NULL
+    ,VistaNombreEsquema		    NVARCHAR(100) NOT NULL
+    ,VistaNombreOrigen  		NVARCHAR(100) NOT NULL
+
+    ,Estado				NVARCHAR(1) NOT NULL DEFAULT('A')
+    ,FechaCreacion		DATETIME	NOT NULL DEFAULT(GETDATE())
+    ,FechaModifica		DATETIME	NOT NULL DEFAULT(GETDATE())  
+    ,IdUserCreacion		INT			NOT NULL DEFAULT(0)
+    ,IdUserModifica		INT			NOT NULL DEFAULT(0)  
+  
+    ,CONSTRAINT  [PK_EV_IdEsquemaVista]		    PRIMARY KEY CLUSTERED (IdEsquemaVista) 
+    ,CONSTRAINT   FK_EV_IdHomologacionEsquema	FOREIGN KEY (IdHomologacionEsquema) REFERENCES HomologacionEsquema(IdHomologacionEsquema)
+    ,CONSTRAINT   FK_EV_IdConexion              FOREIGN KEY (IdConexion)				REFERENCES Conexion (IdConexion)
+    ,CONSTRAINT  [CK_EV_Estado]					CHECK   (Estado IN ('A', 'X'))
+  
+);
+
 DROP TABLE if exists [dbo].[LogMigracion] ;
 CREATE TABLE [dbo].[LogMigracion]  (
-    IdLogMigracion		INT IDENTITY(1,1)			-- Identificador único para el registro del log
-    ,MigracionNumero	INT				NOT NULL DEFAULT 0 		    -- determina la secuencia de migración realizada
-    ,MigracionEstado	NVARCHAR(10)    NOT NULL DEFAULT ('')		-- Estado de la migración (Ejemplo: 'OK', 'ERROR','START')
-	,MigracionTipo 		NVARCHAR(10)    NOT NULL DEFAULT ('')      -- Si la migracion se realiza por: APP o SQLSERVER
-    ,OrigenFormato		NVARCHAR(10)    NOT NULL DEFAULT ('')      -- Nombre del formato-origen de los datos
-    ,OrigenSistema		NVARCHAR(100)   NOT NULL DEFAULT ('')     -- Nombre del sistema que provee los datos
-    ,OrigenVista		NVARCHAR(100)   NOT NULL DEFAULT ('')     -- Nombre de la vista de origen
-    ,OrigenTotalFila	INT             NOT NULL DEFAULT 0                    -- Número de registros a migrar
-    ,IdHomologacionEsquema  INT         NOT NULL DEFAULT 0  
-    ,VistaNombreEsquema		NVARCHAR(255)   NOT NULL DEFAULT ('')		-- Migrar segun nombre de vista de HomologacionEsquema (VistaNombre) 
-    ,TotalFila			INT				NOT NULL DEFAULT 0                   -- Número de registros migrados
-    ,TiempoInicio		DATETIME		NOT NULL DEFAULT GETDATE()          -- Fecha y hora de inicio de la migración
-    ,TiempoFinal		DATETIME		NOT NULL DEFAULT GETDATE()          -- Fecha y hora de finalización de la migración
-	,TiempoTotal        AS 
+     IdLogMigracion		INT IDENTITY(1,1)						-- Identificador único para el registro del log
+    ,IdConexion			INT         	NOT NULL            -- campo de la tabla conexcion.IdConexion
+	,CodigoHomologacion	NVARCHAR(20)	NOT NULL                -- campo de la tabla conexcion.CodigoHomologacion
+    ,Host               NVARCHAR(100)   NOT NULL                -- campo de la tabla conexcion.Host
+    ,Puerto             INT             NOT NULL DEFAULT(0)     -- campo de la tabla conexcion.Puerto
+    ,Usuario            NVARCHAR(100)   NOT NULL                -- campo de la tabla conexcion.Usuario
+    ,Migracion			INT				NOT NULL DEFAULT 0 		-- determina la secuencia de migración realizada
+    ,Estado				NVARCHAR(10)    NOT NULL DEFAULT ('')	-- Estado de la migración (Ejemplo: 'OK', 'ERROR','START')
+    ,OrigenFormato		NVARCHAR(10)    NOT NULL DEFAULT ('')	-- Nombre del formato-origen de los datos
+    ,OrigenSistema		NVARCHAR(100)   NOT NULL DEFAULT ('')	-- Nombre del sistema que provee los datos o Base de datos
+    ,OrigenVista		NVARCHAR(100)   NOT NULL DEFAULT ('')	-- Nombre de la vista de origen
+    ,OrigenFilas		INT             NOT NULL DEFAULT 0		-- Número de registros a migrar
+    ,EsquemaId			INT				NOT NULL DEFAULT 0		-- Id de  [dbo].[HomologacionEsquema]
+    ,EsquemaVista		NVARCHAR(100)   NOT NULL DEFAULT ('')	-- Nombre de vista de HomologacionEsquema (VistaNombre) 
+    ,EsquemaFilas		INT				NOT NULL DEFAULT 0		-- Número de registros migrados
+    ,Tiempo				AS 
 						CASE 
-							WHEN TiempoFinal IS NOT NULL THEN 
-								CONVERT(VARCHAR, DATEDIFF(SECOND, TiempoInicio, TiempoFinal) / 3600) + ':' +
-								RIGHT('0' + CONVERT(VARCHAR, (DATEDIFF(SECOND, TiempoInicio, TiempoFinal) % 3600) / 60), 2) + ':' +
-								RIGHT('0' + CONVERT(VARCHAR, DATEDIFF(SECOND, TiempoInicio, TiempoFinal) % 60), 2)
+							WHEN Final IS NOT NULL THEN 
+								CONVERT(VARCHAR, DATEDIFF(SECOND, Inicio, Final) / 3600) + ':' +
+								RIGHT('0' + CONVERT(VARCHAR, (DATEDIFF(SECOND, Inicio, Final) % 3600) / 60), 2) + ':' +
+								RIGHT('0' + CONVERT(VARCHAR, DATEDIFF(SECOND, Inicio, Final) % 60), 2)
 							ELSE 
 								NULL 
-						END
-    ,MensageError		NVARCHAR(MAX) NOT NULL DEFAULT ('')              -- Detalle de cualquier error ocurrido de la migración
-    ,FechaCreacion		DATETIME	NOT NULL DEFAULT GETDATE()
+						END											-- Tiempo de migracion > 
+    ,Inicio				DATETIME		NOT NULL DEFAULT GETDATE()	-- Fecha y hora de inicio de la migración
+    ,Final				DATETIME		NOT NULL DEFAULT GETDATE()	-- Fecha y hora de finalización de la migración
+    ,Fecha				DATETIME		NOT NULL DEFAULT GETDATE()	-- fecha de creación del registro
+	,Observacion		NVARCHAR(MAX)	NOT NULL DEFAULT ('')		-- Detalle de cualquier error ocurrido de la migración
   
     ,CONSTRAINT  [PK_LM_IdLogMigracion]		PRIMARY KEY CLUSTERED (IdLogMigracion) 
-    ,CONSTRAINT  [CK_LM_MigracionEstado]	CHECK   (MigracionEstado	IN ('OK', 'ERROR','START','BATCH',''))
-    ,CONSTRAINT  [CK_LM_MigracionTipo]		CHECK   (MigracionTipo		IN ('APP', 'SQLSERVER',''))
-    ,CONSTRAINT  [CK_LM_OrigenFormato]		CHECK   (OrigenFormato		IN ('MYSQL', 'SQLSERVER', 'SQLLITE','EXCEL',''))
+    ,CONSTRAINT  [CK_LM_Estado]	            CHECK   (Estado			IN ('OK', 'ERROR','START','BATCH',''))
+    ,CONSTRAINT  [CK_LM_OrigenFormato]		CHECK   (OrigenFormato	IN ('MYSQL', 'SQLSERVER', 'SQLLITE','EXCEL',''))
+);
+
+DROP TABLE if exists [dbo].[LogMigracionDetalle] ;
+CREATE TABLE [dbo].[LogMigracionDetalle](
+	 IdLogMigracionDetalle		INT IDENTITY(1,1)	NOT NULL 
+	,IdLogMigracion		        INT             	NOT NULL 
+	,NroMigracion				INT					NOT NULL 
+	,EsquemaId					INT					NOT NULL 
+	,EsquemaVista				[nvarchar](100)		NOT NULL 
+	,EsquemaIdHomologacion		[nvarchar](10)	    NULL 
+	,[IdHomologacion]			[int]				NULL 
+	,[NombreHomologacion]		[nvarchar](90)		NULL 
+	,OrigenVistaColumna			[sysname]			NULL
+	,Fecha				        DATETIME		    NOT NULL DEFAULT GETDATE()	-- fecha de creación del registro
+	,Estado				        NVARCHAR(1)         NOT NULL DEFAULT('A')		 
+  
+    ,CONSTRAINT  [PK_LMD_IdLogMigracionDetalle]		PRIMARY KEY CLUSTERED (IdLogMigracionDetalle) 
+) ON [PRIMARY]
+GO
+
+DROP TABLE IF EXISTS LogMigracionVista
+CREATE TABLE LogMigracionVista (	
+    NroFila     INT, 
+	OrigenVista NVARCHAR(100),  
+	JsonData    NVARCHAR(MAX)
 );
 
 EXEC DBO.Bitacora 'CREATE TABLE
