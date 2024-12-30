f770ddcf808755a9d759224271c3d73e4d2fbf19
MaykinTec2024 <maykinIV16@outlook.com>
2024-12-29 19:09:54 -0500

    20241229_Apis

--- ClientApp/Helpers/Inicializar.cs
+++ ClientApp/Helpers/Inicializar.cs
@@ -2,8 +2,9 @@ namespace ClientApp.Helpers
 {
     public static class Inicializar
     {
-        public const string UrlBaseApi = "https://localhost:7116/";
-        public const string UrlBaseApiBa = "https://localhost:7154/";
+        //public const string UrlBaseApi = "https://localhost:7116/";
+        public const string UrlBaseApi = "https://localhost:7154/";
+        //public const string UrlBaseApiBa = "https://localhost:7154/";
         public const string Token_Local = "JWT Token";
         public const string Datos_Usuario_Local = "Detalle Usuario";
         public const string Datos_Usuario_Rol_Local = "Rol Usuario";
