f11dd572d9ce025e490e9e36eb73066e2b6949b4
pat_mic <pat_mic@hotmail.com>
2024-12-31 09:48:38 -0500

    actualizacion de EsquemaDto

--- SharedApp/Models/Dtos/EsquemaVista.cs
+++
@@ -1,9 +0,0 @@
-namespace SharedApp.Models.Dtos
-{
-  public class EsquemaVista
-  {
-    public string? NombreEsquema { get; set; }
-    public string? NombreVista { get; set; }
-    public bool IsValid { get; set; }
-  }
-}
