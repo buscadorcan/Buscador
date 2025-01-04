531dd99e75ca16cd1b4bdddf826bc390b134cbe2
jquintanillac <juanqcalderon@gmail.com>
2025-01-04 08:14:56 -0500

    Atualizacion del sistema

--- ClientApp/Pages/Administracion/Conexion/Formulario.razor.cs
+++ ClientApp/Pages/Administracion/Conexion/Formulario.razor.cs
@@ -31,27 +31,27 @@ namespace ClientApp.Pages.Administracion.Conexion
                 listaOrganizaciones = await iHomologacionService.GetHomologacionsAsync();
             }
             if (listaVwHomologacion == null && iCatalogosService != null)
-                listaVwHomologacion = await iCatalogosService.GetHomologacionAsync<List<HomologacionDto>>("dimension");
+                listaVwHomologacion = await iHomologacionService.GetHomologacionsAsync();
 
             if (Id > 0 && service != null)
             {
-                var conexion = "";//await service.GetConexionAsync(Id.Value);
-                try
-                {
-                    var ids = JsonConvert.DeserializeObject<List<int>>(conexion ?? "[]");
-                    foreach (var item in ids ?? [])
-                    {
-                        var h = listaVwHomologacion?.FirstOrDefault(c => c.IdHomologacion == item);
-                        if (h != null)
-                        {
-                            lista = lista?.Append(h).ToList();
-                        }
-                    }
-                }
-                catch (System.Exception ex)
-                {
-                    Console.WriteLine(ex);
-                }
+                //var conexion = await service.GetConexionsAsync();
+                //try
+                //{
+                //    var ids = JsonConvert.DeserializeObject<List<int>>(conexion ?? "[]");
+                //    foreach (var item in ids ?? [])
+                //    {
+                //        var h = listaVwHomologacion?.FirstOrDefault(c => c.IdHomologacion == item);
+                //        if (h != null)
+                //        {
+                //            lista = lista?.Append(h).ToList();
+                //        }
+                //    }
+                //}
+                //catch (System.Exception ex)
+                //{
+                //    Console.WriteLine(ex);
+                //}
             }
             else
             {
