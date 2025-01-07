11dfb99f29f1a8dcec6d945531f64e2650c11a2c
jquintanillac <juanqcalderon@gmail.com>
2025-01-06 21:45:21 -0500

    Editar y grabar de ONAConexion

--- WebApp/Models/ONAConexion.cs
+++ WebApp/Models/ONAConexion.cs
@@ -3,26 +3,33 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace WebApp.Models
 {
-  public class ONAConexion : BaseEntity
-  {
-    [Key]
-    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
-    public int IdONA { get; set; }
-    [Required]
-    public string? Host { get; set; }
-    [Required]
-    public int Puerto { get; set; }
-    [Required]
-    public string? Usuario { get; set; }
-    [Required]
-    public string? Contrasenia { get; set; }
-    [Required]
-    public string? BaseDatos { get; set; }
-    [Required]
-    public string? OrigenDatos { get; set; }
-    [Required]
-    public string? Migrar { get; set; }
-    [Required]
-    public string? Estado { get; set; }
-  }
+    public class ONAConexion : BaseEntity
+    {
+        [Key]
+        public int IdONA { get; set; }
+        [Required]
+        public string? Host { get; set; }
+        [Required]
+        public int Puerto { get; set; }
+        [Required]
+        public string? Usuario { get; set; }
+        [Required]
+        public string? Contrasenia { get; set; }
+        [Required]
+        public string? BaseDatos { get; set; }
+        [Required]
+        public string? OrigenDatos { get; set; }
+        [Required]
+        public string? Migrar { get; set; }
+        [Required]
+        public string? Estado { get; set; }
+        [Required]
+        public int? IdUserCreacion { get; set; }
+
+        [Required]
+        public int? IdUserModifica { get; set; }
+
+        [ForeignKey("IdONA")]
+        public virtual ONA? ONA { get; set; }
+    }
 }
--- WebApp/Repositories/ONAConexionRepository.cs
+++ WebApp/Repositories/ONAConexionRepository.cs
@@ -6,50 +6,55 @@ using WebApp.Service.IService;
 
 namespace WebApp.Repositories
 {
-  public class ONAConexionRepository : BaseRepository, IONAConexionRepository
-  {
-    private readonly IJwtService _jwtService;
-    public ONAConexionRepository(
-      IJwtService jwtService,
-      ILogger<ONAConexionRepository> logger,
-      ISqlServerDbContextFactory sqlServerDbContextFactory
-    ) : base(sqlServerDbContextFactory, logger)
+    public class ONAConexionRepository : BaseRepository, IONAConexionRepository
     {
-      _jwtService = jwtService;
-    }
-    public bool Create(ONAConexion data)
-    {
-      data.IdUserCreacion = _jwtService.GetUserIdFromToken(_jwtService.GetTokenFromHeader() ?? "");
-      data.IdUserModifica = data.IdUserCreacion;
+        private readonly IJwtService _jwtService;
+        public ONAConexionRepository(
+          IJwtService jwtService,
+          ILogger<ONAConexionRepository> logger,
+          ISqlServerDbContextFactory sqlServerDbContextFactory
+        ) : base(sqlServerDbContextFactory, logger)
+        {
+            _jwtService = jwtService;
+        }
+        public bool Create(ONAConexion data)
+        {
+            data.IdUserCreacion = _jwtService.GetUserIdFromToken(_jwtService.GetTokenFromHeader() ?? "");
+            data.IdUserModifica = data.IdUserCreacion;
+            data.FechaCreacion = DateTime.Now;
+            data.FechaModifica = data.FechaCreacion;
+            data.Estado = "A";
 
-      return ExecuteDbOperation(context => {
-        context.ONAConexion.Add(data);
-        return context.SaveChanges() >= 0;
-      });
-    }
-    public ONAConexion? FindById(int id)
-    {
-      return ExecuteDbOperation(context => context.ONAConexion.AsNoTracking().FirstOrDefault(u => u.IdONA == id));
-    }
-    public ONAConexion? FindByIdONA(int IdONA)
-    {
-      return ExecuteDbOperation(context => context.ONAConexion.AsNoTracking().FirstOrDefault(u => u.IdONA == IdONA));
-    }
-    public List<ONAConexion> FindAll()
-    {
-      return ExecuteDbOperation(context => context.ONAConexion.AsNoTracking().Where(c => c.Estado.Equals("A")).OrderBy(c => c.FechaCreacion).ToList());
-    }
-    public bool Update(ONAConexion newRecord)
-    {
-      return ExecuteDbOperation(context => {
-        var _exits = MergeEntityProperties(context, newRecord, u => u.IdONA == newRecord.IdONA);
+            return ExecuteDbOperation(context =>
+            {
+                context.ONAConexion.Add(data);
+                return context.SaveChanges() >= 0;
+            });
+        }
+        public ONAConexion? FindById(int id)
+        {
+            return ExecuteDbOperation(context => context.ONAConexion.AsNoTracking().FirstOrDefault(u => u.IdONA == id));
+        }
+        public ONAConexion? FindByIdONA(int IdONA)
+        {
+            return ExecuteDbOperation(context => context.ONAConexion.AsNoTracking().FirstOrDefault(u => u.IdONA == IdONA));
+        }
+        public List<ONAConexion> FindAll()
+        {
+            return ExecuteDbOperation(context => context.ONAConexion.AsNoTracking().Where(c => c.Estado.Equals("A")).OrderBy(c => c.FechaCreacion).ToList());
+        }
+        public bool Update(ONAConexion newRecord)
+        {
+            return ExecuteDbOperation(context =>
+            {
+                var _exits = MergeEntityProperties(context, newRecord, u => u.IdONA == newRecord.IdONA);
 
-        _exits.FechaModifica = DateTime.Now;
-        _exits.IdUserModifica = _jwtService.GetUserIdFromToken(_jwtService.GetTokenFromHeader() ?? "");
+                _exits.FechaModifica = DateTime.Now;
+                _exits.IdUserModifica = _jwtService.GetUserIdFromToken(_jwtService.GetTokenFromHeader() ?? "");
 
-        context.ONAConexion.Update(_exits);
-        return context.SaveChanges() >= 0;
-      });
+                context.ONAConexion.Update(_exits);
+                return context.SaveChanges() >= 0;
+            });
+        }
     }
-  }
 }
