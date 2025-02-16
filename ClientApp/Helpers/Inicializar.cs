cde58c2c3a10c9d05b3be5ba66e2509f7655b6c3
jquintanillac <juanqcalderon@gmail.com>
2025-02-16 11:38:27 -0500

    Formato HTML y esquemas

--- ClientApp/Helpers/Inicializar.cs
+++ ClientApp/Helpers/Inicializar.cs
@@ -5,8 +5,8 @@ namespace ClientApp.Helpers
         //public const string UrlBaseApi = "https://localhost:7116/"; 
         //public const string UrlBaseApi = "http://216.172.100.184/Webapp/";
         //public const string UrlBaseApi = "https://localhost:7154/";
-        public const string UrlBaseApi = "http://216.172.100.184/Webapp/";
-        //public const string UrlBaseApi = "https://localhost:44380/";
+        //public const string UrlBaseApi = "http://216.172.100.184/Webapp/";
+        public const string UrlBaseApi = "https://localhost:44380/";
         //public const string UrlBaseApiBa = "https://localhost:7154/";
         public const string Token_Local = "JWT Token";
         public const string Datos_Usuario_Nombre_Local = "Nombre Usuario";
