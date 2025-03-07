af3364267a7c89200b029d34a6e3bb952ffef715
Apithebest <122989686+APITHEBEST02@users.noreply.github.com>
2025-03-06 23:06:27 -0500

    lurb
    EMAIL SERVICE

--- WebApp/Service/EmailService.cs
+++ WebApp/Service/EmailService.cs
@@ -1,6 +1,11 @@
 using Google.Apis.Gmail.v1;
 using Google.Apis.Gmail.v1.Data;
 using MimeKit;
+using SharedApp.Models.Dtos;
+using System.Net.Mail;
+using System.Runtime.CompilerServices;
+using WebApp.Models;
+using WebApp.Repositories.IRepositories;
 using WebApp.Service.IService;
 
 namespace WebApp.Service
@@ -31,11 +36,18 @@ namespace WebApp.Service
         /// <param name="gmailClientFactory">Factoría para crear instancias de <see cref="GmailService"/>.</param>
         /// <param name="configuration">Configuración de la aplicación.</param>
         /// <param name="logger">Logger.</param>
-        public EmailService(IGmailClientFactory gmailClientFactory, IConfiguration configuration, ILogger<EmailService> logger)
+
+        private readonly IUsuarioEmailRepository _usuarioRepository;
+
+        public EmailService(IGmailClientFactory gmailClientFactory, 
+                            IConfiguration configuration, 
+                            ILogger<EmailService> logger,
+                            IUsuarioEmailRepository usuarioRepository)
         {
             _gmailClientFactory = gmailClientFactory;
             _configuration = configuration;
             _logger = logger;
+            _usuarioRepository = usuarioRepository;
         }
 
         /// <inheritdoc />
@@ -49,7 +61,54 @@ namespace WebApp.Service
                 // Construir el mensaje MIME
                 var message = new MimeMessage();
                 message.From.Add(new MailboxAddress(_configuration["GoogleOAuth:AppName"], _configuration["GoogleOAuth:Username"] ?? throw new InvalidOperationException("Username is required")));
+                
+
                 message.To.Add(new MailboxAddress(to, to));
+                message.Subject = subject;
+                var textPart = new TextPart("html")
+                {
+                    Text = body,
+                    ContentTransferEncoding = ContentEncoding.Base64  // Asegurando que el mensaje se codifique correctamente en base64
+                };
+
+                message.Body = textPart;
+
+                // Convertir MIME a base64 para enviar por Gmail API
+                var rawMessage = Base64UrlEncode(message.ToString());
+
+                // Enviar el mensaje usando Gmail API
+                var gmailMessage = new Message
+                {
+                    Raw = rawMessage
+                };
+
+                await gmailService.Users.Messages.Send(gmailMessage, "me").ExecuteAsync();
+                return true; // Éxito
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Error al enviar el correo a {Email}", to);
+                return false; // Fallo
+            }
+        }
+
+        /// <inheritdoc />
+        public async Task<bool> SendManyEmailAsync(string to, string subject, string body)
+        {
+            try
+            {
+                // Crear servicio de Gmail usando la factoría
+                var gmailService = await _gmailClientFactory.CreateGmailServiceAsync();
+
+                // Construir el mensaje MIME
+                var message = new MimeMessage();
+                message.From.Add(new MailboxAddress(_configuration["GoogleOAuth:AppName"], _configuration["GoogleOAuth:Username"] ?? throw new InvalidOperationException("Username is required")));
+
+                var listaReceptores = to.Split(',');
+                foreach (string direccion in listaReceptores)
+                {
+                    message.To.Add(new MailboxAddress(direccion.Trim(), direccion.Trim()));
+                }
                 message.Subject = subject;
                 var textPart = new TextPart("html")
                 {
@@ -118,7 +177,7 @@ namespace WebApp.Service
 
                     }
                     htmlBody = string.Format(htmlBody, email.usuario);
-                    return await SendEmailAsync(Destinatarios ?? "", "Creación de Usuario", htmlBody);
+                    return await SendManyEmailAsync(Destinatarios ?? "", "Creación de Usuario", htmlBody);
                 }
                 else
                 {
@@ -132,5 +191,7 @@ namespace WebApp.Service
             }
             
         }
+
+
     }
 }
--- _SQLScript/12_EventTracking.sql
+++ _SQLScript/12_EventTracking.sql
@@ -1,18 +1,18 @@
-DROP TABLE IF EXISTS [dbo].[EventTracking]
-GO
+--DROP TABLE IF EXISTS [dbo].[EventTracking]
+--GO
 
-CREATE TABLE [dbo].[EventTracking](
-	 IdEventTracking	INT IDENTITY(1,1) NOT NULL
-	,IdUsuario		    INT NOT NULL DEFAULT(0) 
-	,CodigoHomologacionRol	NVARCHAR(50)  NOT NULL DEFAULT('') 
-	,CodigoHomologacionMenu NVARCHAR(50)  NOT NULL DEFAULT('')
-	,NombreControl		NVARCHAR(100) NOT NULL DEFAULT('')
-	,NombreAccion		NVARCHAR(100) NOT NULL DEFAULT('')
-	,UbicacionJson		NVARCHAR(max) NOT NULL DEFAULT('{}')
-	,ParametroJson		NVARCHAR(max) NOT NULL DEFAULT('{}')
-	,ErrorTracking		NVARCHAR(max) NOT NULL DEFAULT('')
-	,FechaCreacion		DATETIME NOT NULL DEFAULT(GETDATE())  
-	,CONSTRAINT  [PK_ET_IdEventTracking] PRIMARY KEY CLUSTERED (idEventTracking)
-	,CONSTRAINT  [CK_ET_ParametroJson]	 CHECK   (ISJSON(ParametroJson) = 1 )
-);
-GO
+--CREATE TABLE [dbo].[EventTracking](
+--	 IdEventTracking	INT IDENTITY(1,1) NOT NULL
+--	,IdUsuario		    INT NOT NULL DEFAULT(0) 
+--	,CodigoHomologacionRol	NVARCHAR(50)  NOT NULL DEFAULT('') 
+--	,CodigoHomologacionMenu NVARCHAR(50)  NOT NULL DEFAULT('')
+--	,NombreControl		NVARCHAR(100) NOT NULL DEFAULT('')
+--	,NombreAccion		NVARCHAR(100) NOT NULL DEFAULT('')
+--	,UbicacionJson		NVARCHAR(max) NOT NULL DEFAULT('{}')
+--	,ParametroJson		NVARCHAR(max) NOT NULL DEFAULT('{}')
+--	,ErrorTracking		NVARCHAR(max) NOT NULL DEFAULT('')
+--	,FechaCreacion		DATETIME NOT NULL DEFAULT(GETDATE())  
+--	,CONSTRAINT  [PK_ET_IdEventTracking] PRIMARY KEY CLUSTERED (idEventTracking)
+--	,CONSTRAINT  [CK_ET_ParametroJson]	 CHECK   (ISJSON(ParametroJson) = 1 )
+--);
+--GO
