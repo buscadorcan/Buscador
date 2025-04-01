3c491364cb0fa57504c1b3f9b056a6c45fef2955
buscadorServer <buscadorcan@hotmail.com>
2025-04-01 11:46:11 -0600

    borando proyecto de scripts

--- _SQLScript/14_nuevo_elemento_menu.sql
+++
@@ -1,2 +0,0 @@
-﻿	--update x set x.MostrarWeb='Sinonimos',x.TooltipWeb='Sinonimos',x.InfoExtraJson = '{ "icono": "user", "referencia":"/sinonimos"}',x.Estado = 'A'
-	--from Homologacion x where x.IdHomologacion = 29
