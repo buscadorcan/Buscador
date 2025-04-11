fe03f98f077447dffc6b347d9f3debc2c2f72f27
jquintanillac <juanqcalderon@gmail.com>
2025-04-11 11:04:27 -0500

    Filtros anidados

--- SharedApp/Models/Dtos/vw_FiltrosAnidadosDto.cs
+++ SharedApp/Models/Dtos/vw_FiltrosAnidadosDto.cs
@@ -1,13 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace SharedApp.Models.Dtos
 {
+    [Table("vw_FiltrosAnidados")]
     public class vw_FiltrosAnidadosDto
     {
+        [Key]
         public string KEY_FIL_ONA { get; set; }
         public string KEY_FIL_PAI { get; set; }
         public string KEY_FIL_EST { get; set; }
